4ae0425997c05c598dd97a4dfdfd3dbb0bfba768
picuu <carloscapoforcada@gmail.com>
2024-04-27 20:00:50 +0200

    fix: stories clickables

--- components/story.php
+++ components/story.php
@@ -1,7 +1,9 @@
 <article class="story">
-    <div class="story-image">
-        <div class="story-border"></div>
-        <img src=<?= $owner_picture ?> alt=<?= $owner_name ?> />
-    </div>
-    <h3><?= $owner_name ?></h3>
+    <a href="#">
+        <div class="story-image">
+            <div class="story-border"></div>
+            <img src=<?= $owner_picture ?> alt=<?= $owner_name ?> />
+        </div>
+        <h3><?= $owner_name ?></h3>
+    </a>
 </article>
