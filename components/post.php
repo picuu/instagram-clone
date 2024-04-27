cb01681e506dcd41b2b499d0d45f362c85f1f6f6
picuu <carloscapoforcada@gmail.com>
2024-04-27 21:15:48 +0200

    chore: replace text dots with icon in post header

--- components/post.php
+++ components/post.php
@@ -4,7 +4,11 @@
             <img src=<?= $owner_picture ?> alt=<?= $owner_name ?> />
             <h2><?= $owner_name ?></h2>
         </a>
-        <a href="#" class="icon">···</a>
+        <a href="#" class="icon dots-icon">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+            </svg>
+        </a>
     </header>
     <main class="post-content">
         <img src=<?= $image ?> alt=<?= $tags[0] ?> />
